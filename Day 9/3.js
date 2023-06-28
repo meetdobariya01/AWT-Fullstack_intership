9f21ec73dcea940d31126a7ac6846b167287c0cf
meetdobariya01 <137049649+meetdobariya01@users.noreply.github.com>
2023-06-28 15:39:03 +0530

    Add files via upload

---
+++ Day 9/3.js
@@ -0,0 +1,23 @@
+function demoonclick(){
+    alert("On Click");
+}
+
+function demoOnDblclick(){
+    alert("On Double Click");
+}
+
+function demoOnFocus(){
+    document.getElementById("txtFocus").style.backgroundColor="Green";
+}
+
+function demoOnBlur(){
+    document.getElementById("txtBlur").style.backgroundColor="red";
+}
+
+function demoOnKeypress(){
+    alert("Keypress Done");
+}
+
+function demoOnKeyUP(){
+    alert("KeyUP Done");
+}
